2497cbbfffe3463b82d047fd605cb9168644f3fd
Rosyid Haryadi <rosyid_haryadi@protonmail.com>
2025-03-09 20:05:21 +0700

    file naming

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod puzzle_1_length_limit;
-mod puzzle2_grav_wave;
+mod puzzle_2_grav_wave;
 mod puzzle_3_pass_checker;
 
 fn main() {
     puzzle_1_length_limit::length_limit();
-    puzzle2_grav_wave::get_time_gravitational_wave_detected();
+    puzzle_2_grav_wave::get_time_gravitational_wave_detected();
     puzzle_3_pass_checker::count_valid_passwords();
 }
